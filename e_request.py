eba0b8fba69907bc5b71c96899e5d80a80476b1a
Melany Estevez Camata <63152500+Mademoisellefou@users.noreply.github.com>
2021-10-19 16:34:52 -0400

    posibilities
    🔢

--- e_request.py
+++ e_request.py
@@ -1,5 +1,17 @@
+# requests a url  using requests
 import requests
 r = requests.get('https://api.github.com/events')
 data = r.content
 data2= r.text
 print(type(data),type(data2))
+
+# requests a url using selenium
+from selenium import webdriver
+import os
+# path to ChromeDriver.exe
+path = os.path.join('c:\\', 'Users\libia\Downloads\chromedriver\chromedriver.exe')
+#creating a instance
+driver=webdriver.Chrome(path)
+url = 'https://www.foxnews.com/'
+driver.get(url)
+
